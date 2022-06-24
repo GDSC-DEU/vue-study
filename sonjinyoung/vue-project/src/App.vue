828d19837a86bfec0cf7a93916f99110c414fe5c
jinyoung <sm099@naver.com>
2022-06-25 00:05:16 +0900

    05. 함수 사용하기

--- sonjinyoung/vue-project/src/App.vue
+++ sonjinyoung/vue-project/src/App.vue
@@ -1,7 +1,11 @@
 <!-- html 코드 영역(template) -->
 <template>
   <!-- 데이터 바인딩 = {{}} 사용 -->
-  <div class="name">{{name}}</div>
+  <div class="name">
+    <!-- setup()에 있는 함수 직접 호출 시 다음과 같이 {{greeting(name)}} 으로 표현도 가능 -->
+    <!-- setup()에 있는 함수 호출 2 -->
+    {{greet}}
+  </div>
 </template>
 
 <!-- JavaScript 코드 영역(script) -->
@@ -10,9 +14,14 @@ export default {
   // Vue3 = composition API -> setup() 사용
   setup() {
     const name = "jinyoung";
-  
+    const greeting = (name) => {
+      return "Hello " + name;
+    }
+    const greet = greeting(name);
   return {
-    name
+    name,
+    greeting,
+    greet
   };
 }
 }

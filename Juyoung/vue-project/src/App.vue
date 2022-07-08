f0efe52424f68cf242e2fa904237f1aaade0e0dc
ChoiJuyoung <cjy020718@gmail.com>
2022-07-08 13:27:45 +0900

    81. Navbar/Pagenation 컴포넌트 추출

--- Juyoung/vue-project/src/App.vue
+++ Juyoung/vue-project/src/App.vue
@@ -1,17 +1,6 @@
 <template>
 <div>
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <router-link class="navbar-brand" :to="{ name: 'Home'}">JUYOUNG</router-link>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" :to="{ name: 'Todos' }">Todos</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <Navbar />
   <div class="container">
     <router-view/>
   </div>
@@ -21,9 +10,11 @@
 
 <script>
 import Toast from '@/components/Toast';
+import Navbar from '@/components/Navbar'
 export default {
   components: {
-    Toast
+    Toast,
+    Navbar
   },
 }
 </script>
